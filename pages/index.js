49ec9a33e83fed6f4d9dcfef1a9ea908d5874c8c
TheGurufyp <116750570+TheGurufyp@users.noreply.github.com>
2022-11-30 18:14:28 +0500

    new

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,55 @@
-
+import {
+  Box,
+  Flex,
+  Center,
+  Heading,
+  Stack,
+  Input,
+  Select,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { PhoneIcon, Search2Icon } from "@chakra-ui/icons";
 
 export default function Home() {
   return (
-   <>
-    
-   
-   </>
-  )
+    <>
+      <Box>
+        <Center my={"1rem"}>
+          <Heading size={"xl"}>Menu</Heading>
+        </Center>
+        <Flex justify="center" align={"center"}>
+          <Box width={"25%"}>
+            <InputGroup
+              border={"1px"}
+              borderRadius="8px"
+              borderColor={"red.200"}
+            >
+              <InputLeftElement
+                pointerEvents="none"
+                children={<Search2Icon color="black.300" />}
+              />
+              <Input type="text" placeholder="Search Here" />
+            </InputGroup>
+          </Box>
+          <Box width={"25%"} mx="1rem">
+            <Select
+              border={"1px"}
+              borderRadius="8px"
+              borderColor={"orange.200"}
+              placeholder="Categories"
+              _placeholder={{ opacity: 1, color: "gray.500" }}
+            >
+              <option value="option1">All</option>
+              <option value="option2">Sandwich</option>
+              <option value="option3">Burger</option>
+              <option value="option4">Pizza</option>
+              <option value="option5">Drinks</option>
+              <option value="option6">Salad</option>
+            </Select>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
 }
