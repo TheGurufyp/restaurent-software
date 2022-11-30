eb808b5264070a3b613773347d2b147fdbd1abf5
hunfa <nabeelmuzaffar0323@hotmail.com>
2022-11-30 18:28:13 +0500

    c

--- pages/stocks.js
+++ pages/stocks.js
@@ -1,16 +1,48 @@
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Heading, Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer, } from '@chakra-ui/react'
 import React from 'react'
 
 function Stocks() {
   return (
     <>
-    <Heading  border={"1px"}>Stocks</Heading>
-    <Box border={"1px"}>
-hunfa
+    <Box mt="20px" maxW={"1500px"} mx="auto">
+    <Heading textAlign={"center"} >Stocks</Heading>
+    
+   
+    <TableContainer>
+  <Table variant='striped' colorScheme='teal'>
+    
+    <Thead>
+      <Tr>
+        <Th>Item Name</Th>
+        <Th>Current Quantity</Th>
+        <Th isNumeric>Actions</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td>inches</Td>
+        <Td>millimetres (mm)</Td>
+        <Td isNumeric>25.4</Td>
+      </Tr>
+      
+    </Tbody>
+    <Tfoot>
+     
+    </Tfoot>
+  </Table>
+</TableContainer>
+
 
 
     </Box>
-    
     </>
   )
 }
