52736aec332459fe6ddc0b814a55c6646d194975
hunfa <nabeelmuzaffar0323@hotmail.com>
2022-11-30 18:06:37 +0500

    2

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import Navbar from '../Components/Navbar'
 import '../styles/globals.css'
-
+import { ChakraProvider } from '@chakra-ui/react'
 function MyApp({ Component, pageProps }) {
   return <>
+  <ChakraProvider>
   <Navbar/>
   <Component {...pageProps} />
-
+  </ChakraProvider>
   </>
 }
 
---
+++ pages/stocks.js
@@ -0,0 +1,18 @@
+import { Box, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+function Stocks() {
+  return (
+    <>
+    <Heading  border={"1px"}>Stocks</Heading>
+    <Box border={"1px"}>
+hunfa
+
+
+    </Box>
+    
+    </>
+  )
+}
+
+export default Stocks
