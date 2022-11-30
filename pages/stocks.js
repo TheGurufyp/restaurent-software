552d3b9c86c4a878bdcb3f7ee1d6684eb0adcc00
hunfa <nabeelmuzaffar0323@hotmail.com>
2022-11-30 20:14:58 +0500

    stock page

--- pages/stocks.js
+++ pages/stocks.js
@@ -6,31 +6,74 @@ import { Box, Heading, Table,
     Th,
     Td,
     TableCaption,
-    TableContainer, } from '@chakra-ui/react'
-import React from 'react'
+    TableContainer,
+    Button,
+    Text,
+    HStack,
+    Input, } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 function Stocks() {
+
+const [newQuantity, setnewQuantity] = useState(0)
+
+const handlechange=(e)=>{
+ 
+  if(e.target.value[0]==='0')
+{
+  e.target.value=e.target.value.slice(1);
+  console.log(e.target.value)
+}
+  if(e.target.value=== '')
+  {setnewQuantity(0);}
+  else
+ { setnewQuantity(parseInt(e.target.value))}
+}
+
+const handlePlus=()=>{
+
+  setnewQuantity(newQuantity+1);
+}
+
+const handleMinus=()=>{
+
+  setnewQuantity(newQuantity-1)
+}
+
   return (
     <>
-    <Box mt="20px" maxW={"1500px"} mx="auto">
-    <Heading textAlign={"center"} >Stocks</Heading>
+    <Box mt="20px" maxW={"1500px"} mx="auto" px="20px">
+    <Heading mb="20px" textAlign={"center"} position="relative">Stocks
+    <Button colorScheme={"blue"} position={"absolute"} right="10px">Save</Button>
+    </Heading>
     
    
     <TableContainer>
-  <Table variant='striped' colorScheme='teal'>
+  <Table variant='striped' colorScheme='blue'>
     
     <Thead>
       <Tr>
         <Th>Item Name</Th>
-        <Th>Current Quantity</Th>
+        <Th textAlign={"center"}>Current Quantity</Th>
         <Th isNumeric>Actions</Th>
       </Tr>
     </Thead>
     <Tbody>
       <Tr>
-        <Td>inches</Td>
-        <Td>millimetres (mm)</Td>
-        <Td isNumeric>25.4</Td>
+        <Td>Chicken </Td>
+        <Td textAlign={"center"}>20</Td>
+        <Td  >
+         <HStack  justify="end" spacing={4}>
+
+         <Button size={"sm"} colorScheme="green" onClick={()=>{handleMinus()}}>-</Button>
+         {/* <Text  display={"inline-block"}>{newQuantity}</Text> */}
+
+         <Input  w="50px" size={"sm"} borderColor="black" _hover={{borderColor:"black"}} type={"number"} fontWeight={"semibold"} value={newQuantity} onChange={(e)=>{handlechange(e)}} />
+
+         <Button size={"sm"} colorScheme="red"  onClick={()=>{handlePlus()}}>+</Button>
+
+         </HStack>
+          </Td>
       </Tr>
       
     </Tbody>
