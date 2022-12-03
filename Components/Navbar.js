520eb5c396ec88c0de7bd198485be11bfd8f4628
TheGurufyp <116750570+TheGurufyp@users.noreply.github.com>
2022-12-03 13:31:24 +0500

    v 1.0.1

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,41 +1,57 @@
-import { Box,Flex } from '@chakra-ui/react'
-import React,{useState} from 'react'
+import { Box, Flex, Image } from "@chakra-ui/react";
+import React, { useState } from "react";
 import SideBar from "./Sidebar";
 import HamburgerSign from "./hamburgerSign";
 
 function Navbar() {
-
-  const [openSidebar, setopenSidebar] = useState(false)
+  const [openSidebar, setopenSidebar] = useState(false);
 
   return (
     <>
-    <Flex
-      border={"1px"}
-      direction="column"
-      justify={"center"}
-      align="center"
-      bg={"black"}
-      p="0.5rem"
-    >
-       <SideBar  setopenSidebar={setopenSidebar} openSidebar={openSidebar}/>
-       <Box ml="20px" position={"absolute"} left="0px" zIndex="2000" top={"25px"}>
-        <HamburgerSign setopenSidebar={setopenSidebar} openSidebar={openSidebar}/>
-      </Box>
-
-
-      <Box
-        // border={"1px"}
-        color="orange"
-        fontSize={"2rem"}
-        fontWeight="semibold"
-        fontStyle={"italic"}
+      <Flex
+        direction="column"
+        justify={"center"}
+        align="center"
+        bg={"black"}
+        p="0.5rem"
+        borderColor={"white"}
       >
-        Wakhra Swaad
-      </Box>
-     
-    </Flex>
-  </>
-  )
+        <SideBar setopenSidebar={setopenSidebar} openSidebar={openSidebar} />
+        <Box
+          ml="20px"
+          position={"absolute"}
+          left="0px"
+          zIndex="2000"
+          top={"25px"}
+        >
+          <HamburgerSign
+            setopenSidebar={setopenSidebar}
+            openSidebar={openSidebar}
+          />
+        </Box>
+        <Flex justify={"center"} align="center">
+          <Box
+            // border={"1px"}
+            color="orange"
+            fontSize={"2rem"}
+            fontWeight="semibold"
+            fontStyle={"italic"}
+          >
+            Wakhra Swaad
+          </Box>
+          <Box>
+            <Image
+              src="/pics/fire3.png"
+              // border={"1px"}
+              // boxSize="100px"
+              objectFit="cover"
+              height={"50px"}
+            ></Image>
+          </Box>
+        </Flex>
+      </Flex>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- Components/Product.js
+++ Components/Product.js
@@ -1,49 +1,52 @@
-import { React, useState,useEffect,useContext } from "react";
-import { Heading, Center, Box, Flex, Button, Divider,CloseButton  } from "@chakra-ui/react";
+import { React, useState, useEffect, useContext, useRef } from "react";
+import {
+  Heading,
+  Center,
+  Box,
+  Flex,
+  Button,
+  Divider,
+  CloseButton,
+} from "@chakra-ui/react";
 import Actions from "./Actions";
-import { CartContext } from "../context/cart"
+import { CartContext } from "../context/cart";
 
-const Product = ({item,index}) => {
- 
+const Product = ({ item, index }) => {
   const [newQuantity, setnewQuantity] = useState(0);
-  const {cart,setcart}=useContext(CartContext);
+  const { cart, setcart } = useContext(CartContext);
   useEffect(() => {
-    
     setnewQuantity(item.quantity);
-  
   }, []);
-  // useEffect(() => {
-    
-  //   console.log(index)
-  
-  // }, );
 
+  const initialRender = useRef(true);
   useEffect(() => {
-    
-  let tempCart=cart.map((obj,i)=>{
+    if (initialRender.current) {
+      initialRender.current = false;
+    } else {
+      let tempCart = cart.map((obj, i) => {
+        if (i === index) {
+          return {
+            ...obj,
+            quantity: newQuantity,
+            totalPrice: obj.price * newQuantity,
+          };
+        }
 
-     if(i===index)
-     {
-      return {...obj,quantity:newQuantity,totalPrice:(obj.price*newQuantity)};
-     }
+        return obj;
+      });
 
-       return obj;
-  });
+      setcart(tempCart);
+    }
+  }, [newQuantity]);
 
-  setcart(tempCart);
-  
-  }, [newQuantity])
-  
-
-
-  const removefromcart=()=>{
-    let tempCart=cart.map((i)=>{
-       return i;
-    })
+  const removefromcart = () => {
+    let tempCart = cart.map((i) => {
+      return i;
+    });
     tempCart.splice(index, 1);
     setcart(tempCart);
-  }
-  
+  };
+
   return (
     <>
       <Flex
@@ -53,13 +56,13 @@ const Product = ({item,index}) => {
         my="1rem"
       >
         <Box w={"20%"}>{item?.name}</Box>
-        <Box w={"15%"} mr={"2rem"}  ml="15px">
+        <Box w={"15%"} mr={"2rem"} ml="15px">
           <Actions newQuantity={newQuantity} setnewQuantity={setnewQuantity} />
         </Box>
         <Box w={"20%"}>{item?.price} </Box>
         <Box w={"20%"}>{item?.totalPrice}</Box>
         <Box w={"5%"}>
-        <CloseButton size='md' onClick={removefromcart}/>
+          <CloseButton size="md" onClick={removefromcart} />
         </Box>
       </Flex>
     </>
